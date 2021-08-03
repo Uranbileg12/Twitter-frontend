19a8baf3739060e73f57709101741e7e72cc97e0
uranbileg12 <ukaa1168@gmail.com>
2021-08-04 00:23:13 +0800

    login

---
+++ Twitter-test/src/main/java/com/example/Twittertest/TwitterTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.Twittertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterTestApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(TwitterTestApplication.class, args);
+	}
+
+}
